626746631b78b18b4cf7b2595261ba8ded9c9d9c
Todd Strader <todd.strader@gmail.com>
2020-03-20 08:41:37 -0400

    NOCOMMIT -- reducing AST size

--- test_regress/t/t_type_comparison.v
+++ test_regress/t/t_type_comparison.v
@@ -4,38 +4,40 @@
 // without warranty, 2020 by Todd Strader.
 
 module foo
-   #(parameter type a_type = logic,
-     parameter type b_type = int)
+   #(parameter type a_type = logic//,
+     /*parameter type b_type = int*/ )
    ();
 
    initial begin
-      if (type(a_type) != type(logic[7:0])) begin
-         $display("%%Error: a_type is wrong");
+      // TODO -- switch statement
+      //if (type(a_type) != type(logic[7:0]))// begin
+      if (type(a_type) == type(logic[7:0]))// begin
+//         $display("%%Error: a_type is wrong");
          $stop();
-      end
+//      end
 
-      if (type(b_type) != type(real)) begin
-         $display("%%Error: b_type is wrong");
-         $stop();
-      end
-
-      if (type(a_type) == type(logic)) begin
-         $display("%%Error: a_type is the default value");
-         $stop();
-      end
-
-      if (type(b_type) == type(int)) begin
-         $display("%%Error: b_type is the default value");
-         $stop();
-      end
-
-      if (type(a_type) == type(b_type)) begin
-         $display("%%Error: a_type equals b_type");
-         $stop();
-      end
-
-      $write("*-* All Finished *-*\n");
-      $finish;
+//      if (type(b_type) != type(real)) begin
+//         $display("%%Error: b_type is wrong");
+//         $stop();
+//      end
+//
+//      if (type(a_type) == type(logic)) begin
+//         $display("%%Error: a_type is the default value");
+//         $stop();
+//      end
+//
+//      if (type(b_type) == type(int)) begin
+//         $display("%%Error: b_type is the default value");
+//         $stop();
+//      end
+//
+//      if (type(a_type) == type(b_type)) begin
+//         $display("%%Error: a_type equals b_type");
+//         $stop();
+//      end
+//
+//      $write("*-* All Finished *-*\n");
+//      $finish;
    end
 
 endmodule
@@ -43,7 +45,7 @@ endmodule
 module t();
 
    foo #(
-      .a_type (logic[7:0]),
-      .b_type (real)) the_foo ();
+      .a_type (logic[7:0])//,
+      /*.b_type (real) */) the_foo ();
 
 endmodule
